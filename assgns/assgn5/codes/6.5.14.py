d55654f428aa703fd2c8c1714fc1755f4b2eba3c
ee24btech11065 <ytnspoorthi2006@gmail.com>
2025-01-15 15:52:05 +0530

    update

--- assgns/assgn5/codes/6.5.14.py
+++ assgns/assgn5/codes/6.5.14.py
@@ -1,6 +1,28 @@
 import numpy as np
 import ctypes
 import matplotlib.pyplot as plt
+import cvxpy as cp
+
+# Define variables
+x = cp.Variable(pos=True)  # x > 0
+y = cp.Variable(pos=True)  # y > 0
+
+# Define the constraints
+constraints = [x + y == 60]  # Equality constraint
+
+# Define the objective function
+objective = cp.Maximize(cp.log(x) + 3 * cp.log(y))  # Maximize log(x) + 3*log(y)
+
+# Formulate the problem
+problem = cp.Problem(objective, constraints)
+
+# Solve the problem
+problem.solve()
+
+# Extract results
+optimal_x = x.value
+optimal_y = y.value
+optimal_f = optimal_x * (optimal_y ** 3)  # f(x) = x * y^3
 
 # lib stores the shared object file of the c code
 lib = ctypes.CDLL('./6.5.14.so')
@@ -20,14 +42,19 @@ xmax = result[0]
 ymax = result[1]
 
 print("x value at maxima = ",xmax)
-print("y value at maxima = ",ymax)
+print("y value at maxima = ",60 - xmax)
 
 #plotting theoretical curve
 plt.plot(xcoords, ycoords,color='r',linewidth =2,label="functional curve")
 #plot the maxima
-plt.scatter(xmax,ymax,color='k',label='maxima')
+plt.scatter(xmax,ymax,s = 400,color='k',label="sim1 maxima")
 
-
+y1 = (optimal_x)*((optimal_y)**3)
+y2 = 15*45*45*45
+plt.scatter(optimal_x,y1,s = 200,color='g',label="sim2 maxima")
+plt.scatter(15,y2,color='m',s=100,label = "theory maxima")
+print("value for x using geometric programming" ,optimal_x)
+print("value for y using geometric programming",optimal_y)
 #labelling the two perpendicular axes
 plt.xlim(13,17)
 plt.ylim(1349699,1367500)
@@ -37,7 +64,6 @@ plt.legend()
 # To get a grid
 plt.grid(True)
 plt.show()
-
 
 
 
